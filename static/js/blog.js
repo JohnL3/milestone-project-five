785c2676e999071234cde8bf138a134e62f17fa3
John L <johnlonggately@gmail.com>
2018-12-03 12:48:44 +0000

    created javascript file for blog pages

---
+++ static/js/blog.js
@@ -0,0 +1,16 @@
+$('#burger').click(function(){
+    if($('nav').css('display') === 'none'){
+        $('nav').css('display','grid');
+    } else {
+        $('nav').css('display','none');
+    }
+});
+
+$( window ).resize(function() {
+  if($(window).width() > 767) {
+     //$('header').css('display', 'grid');
+	   $('nav').css('display','grid');
+  } else {
+      $('nav').css('display','none');
+  }
+});
