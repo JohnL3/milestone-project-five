477862fe051b5e8758781a787f3a0d384a5594af
John L <johnlonggately@gmail.com>
2018-12-11 13:48:31 +0000

    started adding queries to the bug views.py

--- bugs/views.py
+++ bugs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.utils import timezone
-
+from .models import Bug, BugComment, BugVotes
 
 from django.contrib.auth.decorators import login_required
 
@@ -9,10 +9,16 @@ from django.contrib.auth.decorators import login_required
 def get_bugs(request):
     '''Get a list of all bugs and render them'''
     
-    return render(request,'bugs.html')
+    bugs = Bug.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+    
+    return render(request,'bugs.html', {'bugs': bugs})
 
 
 def bug_details(request, pk):
     '''Create a view that returns a single bug details'''
+    bug = get_object_or_404(Bug, pk=pk)
+    comments = BugComment.objects.filter(BugComment_id=pk)
+    print('id', pk)
+    print('author',bug.bug_author)
     
-    return render(request, 'singlebug.html')
+    return render(request, 'singlebug.html', {'bug': bug}, {'comments': comments})
