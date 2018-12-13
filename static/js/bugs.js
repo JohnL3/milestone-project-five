4dd8c6a91bab2fc0f72b28aba207fa891487de5b
John L <johnlonggately@gmail.com>
2018-12-13 09:52:12 +0000

    added functions so i can use $.ajax to post to singlepost route

--- static/js/bugs.js
+++ static/js/bugs.js
@@ -26,3 +26,55 @@ function changeDateView() {
 }
 
 changeDateView();
+
+
+$('#comment-form').on('submit', function(event){
+    event.preventDefault();
+    console.log("form submitted!");
+    create_comment();
+});
+
+function create_comment() {
+    
+    let data = {};
+ 
+    let bug_id = +$('.bug-id').prop('id');
+    data.comment = $('#comment-text').val();
+    data.csrfmiddlewaretoken = $("[name=csrfmiddlewaretoken]").val();
+    data.created_date = $('#date-comment').val();
+    
+    
+    //console.log('DATE', data.created_date);
+    
+    let url ='/bugs/'+bug_id+"/";
+    
+    $.ajax({
+        url : url,
+        type : "POST", 
+        data : data,
+        success : function(json) {
+            $('#comment-text').val(''); 
+            console.log(json);
+            console.log("successfully done yeeeeeee");
+            createNewComment(json);
+        },
+    }); 
+}
+
+function createNewComment(json) {
+    let surround = `<div class='surround'>
+        <div class='avatar'>
+            <img src='/media/images/admin.jpg' alt='avatar'>
+        </div>
+        <div class='comment-head'>
+            <span class='user-name'>${json.username}</span>
+            <span class='user'>Commented: </span>
+            <span class='user'>${json.created_date}</span>
+        </div>
+        <div class='comment-area'>
+            <p>${json.comment_text}</p>
+        </div>
+    </div>`;
+    
+    $('#all-comments').append(surround);
+}
