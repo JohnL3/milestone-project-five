336e0a8e86a6dc033b53076483d1404ccee77447
John L <johnlonggately@gmail.com>
2018-12-16 10:28:19 +0000

    Added a new url route

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from bugs.views import get_bugs, bug_details, upvote
+from bugs.views import get_bugs, bug_details, upvote, bug_issue
 
 
 urlpatterns = [
     url(r'^$', get_bugs, name='bugs'),
     url(r'^(?P<pk>\d+)/$', bug_details, name='bug_details'),
     url(r'^upvote/$', upvote, name='upvote'),
+    url(r'^issue/$', bug_issue, name='issue'),
 ]
