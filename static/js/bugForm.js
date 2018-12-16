4d6b136353464e041b7169364878e04fe937e096
John L <johnlonggately@gmail.com>
2018-12-16 10:27:30 +0000

    Added a js file for bug_form.html

---
+++ static/js/bugForm.js
@@ -0,0 +1,35 @@
+$('#burger').click(function(){
+    if($('nav').css('display') === 'none'){
+        $('nav').css('display','grid');
+    } else {
+        $('nav').css('display','none');
+    }
+});
+
+$( window ).resize(function() {
+  if($(window).width() > 767) {
+	   $('nav').css('display','grid');
+  } else {
+      $('nav').css('display','none');
+  }
+});
+
+$('.submit-btn').click(function(){
+    event.preventDefault();
+    let data = {};
+    data.bug_title = $('#id_bug_title').val();
+    data.initial_comment = $('#id_initial_comment').val();
+    data.csrfmiddlewaretoken = $("[name=csrfmiddlewaretoken]").val();
+    
+    let url ='/bugs/issue/';
+    
+    $.ajax({
+        url : url,
+        type : "POST", 
+        data : data,
+        success : function(json) {
+            
+        },
+    });
+    
+});
