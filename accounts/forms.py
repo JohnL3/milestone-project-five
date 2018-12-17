67391a216baf31ae497e6ed051cb2ff8c8ff3d00
John L <johnlonggately@gmail.com>
2018-12-17 11:27:18 +0000

    Major update ... extended user model giving user profile via a oneToOneField

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from .models import Profile
+
 
 
 class UserLoginForm(forms.Form):
@@ -46,4 +48,9 @@ class UserRegistrationForm(UserCreationForm):
             raise forms.ValidationError('Passwords must match..')
            
         return password2
-    
+        
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar_url', 'image']
