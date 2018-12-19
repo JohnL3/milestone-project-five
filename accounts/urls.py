ea8bc09122fd1fe8cf257bdc9cfa21214ebfb29b
John L <johnlonggately@gmail.com>
2018-12-19 11:12:50 +0000

    Added a profile url for new profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from accounts.views import index, logout, login, register, register_login
+from accounts.views import index, logout, login, register, register_login, profile
 
 urlpatterns = [
     url(r'^logout/$', logout, name='logout'),
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^register_login/$', register_login, name='register_login'),
     url(r'^login/$', login, name='login'),
     url(r'^register/$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
 ]
