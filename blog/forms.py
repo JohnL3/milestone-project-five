319e695a1b502bbbdf72da4c71f870bf3ddf751f
John L <johnlonggately@gmail.com>
2018-12-31 11:54:47 +0000

    Made changes to .forms so i could remove text sent with form

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Post
+from django.forms.widgets import ClearableFileInput
+
+class MyClearableFileInput(ClearableFileInput):
+    initial_text = ''
+    input_text = ""
+    clear_checkbox_label = 'Clear'
+
 
 class BlogPostForm(forms.ModelForm):
+    image = forms.ImageField(label='Image',required = False, widget=MyClearableFileInput)
     class Meta:
         model = Post
         fields = ("title", "content", "image", "published_date")
+        
