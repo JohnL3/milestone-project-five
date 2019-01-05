ea8852a734de3a26716e8035b6ceade828eec5e5
John L <johnlonggately@gmail.com>
2019-01-05 17:28:04 +0000

    added a blank line

--- blog/forms.py
+++ blog/forms.py
@@ -4,12 +4,13 @@ from django.forms.widgets import ClearableFileInput
 
 class MyClearableFileInput(ClearableFileInput):
     initial_text = ''
-    input_text = ""
+    input_text = ''
     clear_checkbox_label = 'Clear'
 
 
 class BlogPostForm(forms.ModelForm):
     image = forms.ImageField(label='Image',required = False, widget=MyClearableFileInput)
+    
     class Meta:
         model = Post
         fields = ("title", "content", "image", "published_date")
