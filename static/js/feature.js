2e094f4ca32bee49248aac7bfe39e28cb5685949
John L <johnlonggately@gmail.com>
2019-01-16 12:51:15 +0000

    added a function to add feature to cart

--- static/js/feature.js
+++ static/js/feature.js
@@ -13,3 +13,29 @@ $( window ).resize(function() {
       $('nav').css('display','none');
   }
 });
+
+//Add a feature to the cart
+$('.feature-submit-btn').click(function(){
+     event.preventDefault();
+     let data = {};
+     data.item_id = $('.feature-id').prop('id');
+     data.csrfmiddlewaretoken = $("[name=csrfmiddlewaretoken]").val();
+     
+     let url = '/cart/add/'+data.item_id;
+     
+     $.ajax({
+        url : url,
+        type : "POST", 
+        data : data,
+        success : function(data) {
+            console.log(data);
+            let quantity = +$('.badge').text()+1;
+            $('.badge').text(quantity);
+            
+            $('.feature-success-msg').text('Added to cart');
+            setTimeout(function(){
+                $('.feature-success-msg').text('');
+            },2000);
+        },
+    });
+})
